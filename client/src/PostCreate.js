f09abd4dec9580c58581e98c11c52d1d134a2749
CrimsonCoder42 <nostro37@gmail.com>
2023-01-14 06:20:39 -0500

    day 12

---
+++ client/src/PostCreate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return <div>
+    <form>
+    <div className="form-group">
+    <label>Title</label>
+    <input className="form-control">
+    </div>
+    </form>
+  </div>;
+}
