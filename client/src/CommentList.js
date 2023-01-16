72c7a3494d6502dc564f532640d7d4818d703683
CrimsonCoder42 <nostro37@gmail.com>
2023-01-16 10:49:39 -0500

    comment list created

---
+++ client/src/CommentList.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+export default ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchData = async () => {
+    const res = await axios.get(`http://localhost:4001/posts/${postID}/comments`)
+
+    setComments(res.data);
+  }; 
+
+  return <div/>
+}
