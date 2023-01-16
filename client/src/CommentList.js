7ab0dac27397ce03e7584860076c867e32869861
CrimsonCoder42 <nostro37@gmail.com>
2023-01-16 11:17:28 -0500

    comments updated

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default ({ postId }) => {
+const CommentList = ({ postId }) => {
   const [comments, setComments] = useState([]);
 
   const fetchData = async () => {
-    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
 
     setComments(res.data);
-  }; 
+  };
 
   useEffect(() => {
     fetchData();
-  })
+  }, []);
 
-  const renderedComments = comments.map(comment => {
-    return <li key={comment.id}> {comment.content} </li>;
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
   });
 
-  return <ul> 
-    {renderedComments}
-  </ul>
-}
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
